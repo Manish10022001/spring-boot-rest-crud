9aff70e437fa4fcc2565b4a6248888c65ce257af
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-23 12:30:48 +0530

    updated service implementation to delegate calls to DAO

--- 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.springboot.cruddemo.service;
 //Step 6
 import com.springboot.cruddemo.dao.EmployeeDAO;
 import com.springboot.cruddemo.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,5 +20,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll(){
         return employeeDAO.findAll();
+    }
+
+    //step 10: add methods and delegate calls to dao
+    @Override
+    public Employee findById(int theId){
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional //as we update or insert new values, and gave @transaction here instead of DAO methods
+    public Employee save(Employee theEmployee){
+        return employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional // as value changes
+    public void deleteById(int theId){
+        employeeDAO.deleteById(theId);
     }
 }
