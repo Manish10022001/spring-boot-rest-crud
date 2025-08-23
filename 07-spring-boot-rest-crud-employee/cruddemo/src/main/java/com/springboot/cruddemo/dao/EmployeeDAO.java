b94dbcf27572ea9479497adf8db4118ecec57843
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-23 12:29:07 +0530

    Updated employeeDAO by adding new methods

--- 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeDAO.java
+++ 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/dao/EmployeeDAO.java
@@ -6,4 +6,12 @@ import java.util.List;
 
 public interface EmployeeDAO {
     List<Employee> findAll();
+
+    //Step 7-> add new methods-> findbyid, add, delete , update
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
 }
