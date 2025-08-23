b91759ecdd63fcdf7fcc727782b8d1134306f8e2
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-23 12:30:03 +0530

    updated employee service

--- 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/service/EmployeeService.java
+++ 07-spring-boot-rest-crud-employee/cruddemo/src/main/java/com/springboot/cruddemo/service/EmployeeService.java
@@ -6,4 +6,9 @@ import java.util.List;
 
 public interface EmployeeService {
     List<Employee> findAll();
+
+    //Step 9: add methods
+    Employee findById(int theId);
+    Employee save(Employee theEmployee);
+    void deleteById(int theId);
 }
